f38be0af6968da3da4b9bd7e16c39ed2edaf1067
gazdi435 <zsoltgazdag444@gmail.com>
2024-12-17 19:20:33 +0100

    Add project files.

---
+++ AtvaltOOP/Atvalt.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtvaltOOP
+{
+    internal class Atvalt
+    {
+        public Atvalt()
+        {
+            
+        }
+
+        public string BinarisraValtStringes(int megadottSzam)
+        {
+            return Convert.ToString(BinarisraValt(megadottSzam));
+        }
+
+        public string BinarisraValtStringes(string megadottSzam)
+        {
+            try
+            {
+                int szam = Convert.ToInt32(megadottSzam);
+                return Convert.ToString(BinarisraValt(szam));
+            }
+            catch
+            {
+                throw new Exception("Nem érvényes decimális szám!");
+            }
+        }
+
+        public int BinarisraValt(int megadottSzam)
+        {
+            if (megadottSzam < 0)
+            {
+                throw new ArgumentException("Csak nem-negatív számokat lehet binárisra váltani.");
+            }
+
+            int binarisSzam = 0;
+            int helyiertek = 1;
+
+            while (megadottSzam > 0)
+            {
+                int maradek = megadottSzam % 2;       
+                binarisSzam += maradek * helyiertek;  
+                megadottSzam /= 2;                    
+                helyiertek *= 10;                     
+            }
+
+            return binarisSzam;
+        }
+
+        public string DecimalisbaValtStringes(int megadottSzam)
+        {
+
+            return Convert.ToString(DecimalisbaValt(megadottSzam));
+        }
+
+
+
+        public string DecimalisbaValtStringes(string megadottSzam)
+        {
+            string binarisSzam = Convert.ToString(megadottSzam);
+
+            return Convert.ToString(DecimalisbaValt(binarisSzam));
+        }
+
+        public int DecimalisbaValt(int megadottSzam)
+        {
+            string binarisSzam = Convert.ToString(megadottSzam);
+
+            return DecimalisbaValt(binarisSzam);
+        }
+
+        public int DecimalisbaValt(string binarisSzam)
+        {
+            if (Convert.ToInt32(binarisSzam) < 0)
+            {
+                throw new Exception("A szám nem lehet kissebb mint 0");
+            }
+            try
+            {
+                Convert.ToInt32(binarisSzam);
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception("Nem szám!");
+            }
+            int atvaltottSzam = 0;
+
+            for (int i = 0; i < binarisSzam.Length; i++)
+            {
+                char bit = binarisSzam[binarisSzam.Length - 1 - i]; 
+                if (bit == '1')
+                {
+                    atvaltottSzam += (int)Math.Pow(2, i);
+                }
+                else if (bit != '0')
+                {
+                    throw new Exception("Nem lehet átfordítani!");
+                }
+            }
+
+            return atvaltottSzam;
+        }
+    }
+}
---
+++ AtvaltOOP/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace AtvaltOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Atvalt atvaltas = new Atvalt();
+
+            // BinarisraValt tesztek
+            Console.WriteLine("---- BinarisraValt tesztek ----");
+            try
+            {
+                int decimálisSzám1 = 5;
+                Console.WriteLine($"{decimálisSzám1} binárisra váltva: {atvaltas.BinarisraValt(decimálisSzám1)}"); // 5 -> 101
+                int decimálisSzám2 = 15;
+                Console.WriteLine($"{decimálisSzám2} binárisra váltva: {atvaltas.BinarisraValt(decimálisSzám2)}"); // 15 -> 1111
+                int decimálisSzám3 = 0;
+                Console.WriteLine($"{decimálisSzám3} binárisra váltva: {atvaltas.BinarisraValt(decimálisSzám3)}"); // 0 -> 0
+                int decimálisSzám4 = -5;
+                Console.WriteLine($"{decimálisSzám4} binárisra váltva: {atvaltas.BinarisraValt(decimálisSzám4)}"); // Hibát kell dobjon
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Hiba: {ex.Message}");
+            }
+
+            // BinarisraValtStringes tesztek
+            Console.WriteLine("\n---- BinarisraValtStringes tesztek ----");
+            try
+            {
+                Console.WriteLine($"'5' binárisra váltva: {atvaltas.BinarisraValtStringes("5")}"); // 5 -> 101
+                Console.WriteLine($"'0' binárisra váltva: {atvaltas.BinarisraValtStringes("0")}"); // 0 -> 0
+                Console.WriteLine($"'-5' binárisra váltva: {atvaltas.BinarisraValtStringes("-5")}"); // Hibát kell dobjon
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Hiba: {ex.Message}");
+            }
+
+            // DecimalisbaValt tesztek
+            Console.WriteLine("\n---- DecimalisbaValt tesztek ----");
+            try
+            {
+                string binárisSzám1 = "101";
+                Console.WriteLine($"{binárisSzám1} decimálisra váltva: {atvaltas.DecimalisbaValt(binárisSzám1)}"); // 101 -> 5
+                string binárisSzám2 = "1111";
+                Console.WriteLine($"{binárisSzám2} decimálisra váltva: {atvaltas.DecimalisbaValt(binárisSzám2)}"); // 1111 -> 15
+                string binárisSzám3 = "0";
+                Console.WriteLine($"{binárisSzám3} decimálisra váltva: {atvaltas.DecimalisbaValt(binárisSzám3)}"); // 0 -> 0
+                string binárisSzám4 = "-101";
+                Console.WriteLine($"{binárisSzám4} decimálisra váltva: {atvaltas.DecimalisbaValt(binárisSzám4)}"); // Hibát kell dobjon
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Hiba: {ex.Message}");
+            }
+
+            Console.WriteLine("\n---- DecimalisbaValtStringes tesztek ----");
+            try
+            {
+                Console.WriteLine($"'101' decimálisra váltva: {atvaltas.DecimalisbaValtStringes("101")}"); // 101 -> 5
+                Console.WriteLine($"'1111' decimálisra váltva: {atvaltas.DecimalisbaValtStringes("1111")}"); // 1111 -> 15
+                Console.WriteLine($"'0' decimálisra váltva: {atvaltas.DecimalisbaValtStringes("0")}"); // 0 -> 0
+                Console.WriteLine($"'-101' decimálisra váltva: {atvaltas.DecimalisbaValtStringes("-101")}"); // Hibát kell dobjon
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Hiba: {ex.Message}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
